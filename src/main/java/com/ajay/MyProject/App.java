fafd13ea15a9a2ab42cb534a551ac31f34fbf432
KhusaaniAjayReddy <ajay.chinna.1230@gmail.com>
2025-01-31 12:41:54 +0530

    second commit

--- src/main/java/com/ajay/MyProject/App.java
+++ src/main/java/com/ajay/MyProject/App.java
@@ -1,13 +1,17 @@
 package com.ajay.MyProject;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+	public void addData(String data)
+	{
+		System.out.print("Daa"+data);
+		
+		if(data.equals("ajay"))
+			System.out.print("Welcome");
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
     }
 }
